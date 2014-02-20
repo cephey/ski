ee8bb8e3c7d0e7da291100cb86f3d957fd808c2e
Andrey Ptitsyn <andrey.ptitsyn86@gmail.com>
2014-02-21 00:21:29 +0400

    добавил вьюшки и пустые шаблоны всех остальных страниц. Плюс добавил активные пункты меню

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,9 +1,18 @@
 from django.views.decorators.cache import cache_page
 from django.conf.urls import patterns, url
 
-from pages.views import IndexView
+from pages.views import IndexView, VeloView, PalatView, SnowView, SkiView, UslovView, ContactView
 
 urlpatterns = patterns(
     '',
     url(r'^$', cache_page(5 * 60)(IndexView.as_view()), name='index'),
+    # url(r'^$', IndexView.as_view(), name='index'),
+
+    url(r'^velosipedy/$', VeloView.as_view(), name='velosipedy'),
+    url(r'^palatki/$', PalatView.as_view(), name='palatki'),
+    url(r'^snoubordy/$', SnowView.as_view(), name='snoubordy'),
+    url(r'^gornye_lyzhi/$', SkiView.as_view(), name='gornye_lyzhi'),
+
+    url(r'^uslovia/$', UslovView.as_view(), name='uslovia'),
+    url(r'^contacts/$', ContactView.as_view(), name='contacts'),
 )
--- src/pages/views.py
+++ src/pages/views.py
@@ -13,9 +13,60 @@ class IndexView(TemplateView):
 
         api = WeatherAPI()
         context['weather'] = api.widget()
-
         context['services'] = ServicesText.objects.all()[:4]
-        context['address'] = '420095 г. Казань, ул. Горьковское шоссе 49 к2'
-        context['phones'] = '8 (843) 225-45-15, 8 917 252 45 15'
+
+        return context
+
+
+class VeloView(TemplateView):
+    template_name = 'pages/velosiped.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(VeloView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class PalatView(TemplateView):
+    template_name = 'pages/palatki.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PalatView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class SnowView(TemplateView):
+    template_name = 'pages/snowboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SnowView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class SkiView(TemplateView):
+    template_name = 'pages/mountin_ski.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SkiView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class UslovView(TemplateView):
+    template_name = 'pages/uslovia.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UslovView, self).get_context_data(**kwargs)
+
+        return context
+
+
+class ContactView(TemplateView):
+    template_name = 'pages/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
 
         return context
