ffb7b37aed555d6eabad87b74972a66150a0cf8b
Andrey Ptitsyn <andrey.ptitsyn86@gmail.com>
2014-04-14 19:01:28 +0400

    уменьшил время кеша погоды(sunrise, sunset) до 1 часа

--- src/weather/helpers.py
+++ src/weather/helpers.py
@@ -106,8 +106,7 @@ def get_sunrise_and_sunset():
                 'sunrise': sunrise.strftime('%Y-%m-%d %H:%M:%S'),
                 'sunset': sunset.strftime('%Y-%m-%d %H:%M:%S')
             }
-        # cache.set('weather_sun', data, timeout=24 * 60 * 60)
-        cache.set('weather_sun', data, timeout=60)
+        cache.set('weather_sun', data, timeout=1 * 60 * 60)
 
     now = data[date.today().strftime('%Y-%m-%d')]
     return {
